c4319a6f8ca8460c2599253a80750083b196cbcf
Bryan H <ilikespam321@gmail.com>
2016-05-05 07:11:31 -0700

    added template files from front end to django, created dumby urls/views

--- arientosend/arientosend/urls.py
+++ arientosend/arientosend/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import include, url
+# from django.contrib import admin
+from send import views
 
+# would be nice to not use .\html, looks bad.
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    url(r'^', views.index),
+    url(r'^client\.html', views.client),
+    url(r'^guest\.html', views.guest),
+    url(r'^login\.html', views.login),
 ]
--- arientosend/send/views.py
+++ arientosend/send/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+def client(request):
+    return render(request, 'client.html', {})
+
+def guest(request):
+    return render(request, 'guest.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
