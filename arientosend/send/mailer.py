abb8c201a8706eb6345c05e8dc37ec828e985f24
Bryan H <ilikespam321@gmail.com>
2016-05-10 15:07:16 -0700

    added mailer class

---
+++ arientosend/send/mailer.py
@@ -0,0 +1,16 @@
+import smtplib
+
+GOOGLE_ACCOUNT = "noreply@arientosend.com"
+GOOGLE_PASSWORD = "passwordhere"
+
+class emailer:
+    # maybe rewrite this class to have email connection persist throughout emails
+    def sendmail(content, subject, to_email):
+
+        message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (GOOGLE_ACCOUNT, ", ".join(to_email), subject, content)
+        
+        mail = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        mail.login(GOOGLE_ACCOUNT,GOOGLE_PASSWORD)
+        mail.sendmail(GOOGLE_ACCOUNT, to_email, message)
+        mail.close()
