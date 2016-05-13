6342f13efe9b48ee585810867636b94e58879d0d
Bryan H <ilikespam321@gmail.com>
2016-05-12 21:04:58 -0700

    bug fix in mailer

--- arientosend/send/mailer.py
+++ arientosend/send/mailer.py
@@ -6,10 +6,10 @@ GOOGLE_PASSWORD = getattr(settings, "GOOGLE_PASSWORD", None)
 
 class emailer:
     # maybe rewrite this class to have email connection persist throughout emails
-    def sendmail(to_email, subject, content):
+    def sendmail(self, to_email, subject, content):
 
         message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (GOOGLE_ACCOUNT, ", ".join(to_email), subject, content)
+    """ % (GOOGLE_ACCOUNT, to_email, subject, content)
         
         mail = smtplib.SMTP_SSL("smtp.gmail.com", 465)
         mail.login(GOOGLE_ACCOUNT,GOOGLE_PASSWORD)
