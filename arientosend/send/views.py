3a689d2e1a39bbcdf873ee8e284c1623896ec2c2
EC2 Default User <ec2-user@ip-172-31-9-75.ec2.internal>
2016-05-24 05:32:37 +0000

    Multiple file upload

--- arientosend/send/views.py
+++ arientosend/send/views.py
@@ -46,19 +46,19 @@ def client(request):
 			user = User.objects.get(email=email)
 		except ObjectDoesNotExist:
 			del request.session['authorized_user']
-			return render(request, 'index.html', {})
+			return render(request, 'login.html', {})
 	else:
 		try:
 			email = request.POST['login']
 			# TODO: SafeNet authentication
 			password = request.POST['password']
 		except KeyError:
-			return render(request, 'index.html', {})
+			return render(request, 'login.html', {})
 
 		try:
 			user = User.objects.get(email=email)
 		except ObjectDoesNotExist:
-			return render(request, 'index.html', {})
+			return render(request, 'login.html', {})
 		else:
 			request.session['authorized_user'] = email
 
@@ -72,45 +72,65 @@ def client(request):
 	return HttpResponse(template.render(context, request))
 
 def client_send(request):
+	email = ""
+	if 'authorized_user' in request.session:
+		email = request.session['authorized_user']
+		try:
+			user = User.objects.get(email=email)
+		except ObjectDoesNotExist:
+			del request.session['authorized_user']
+			return render(request, 'login.html', {})
+	else:
+		return render(request, 'login.html', {})
+
 	try:
 		recipient = request.POST['email']
 		password = request.POST['password']
 		message = request.POST['message']
-		# can only get one file at a time
-		attachment = request.FILES['attachments']
+		attachment = request.FILES.getlist('attachments')
 	except KeyError:
 		return redirect('/client')
 	else:
-		af = ArientoFile()
-		af.file=attachment
-		af.save()
+		client_is_recipient = False
+		sent_to_client = False
+		for attached in attachment:
+			af = ArientoFile()
+			af.file=attached
+			af.save()
 
-		fa = FileAccess()
-		fa.file = af
-		fa.recipient_email = recipient
-		fa.sender_email = 'test@ariento.com'
-		try:
-			u = User.objects.get(email=recipient)
-		except ObjectDoesNotExist:
-			fa.access_type = 'P'
-			fa.salt = get_salt()
-			fa.hashed_password = hashed_password(password, fa.salt)
-			email_body = '''An Ariento client, %s, has sent you a file.\nClick here to download it:http://ec2-54-172-241-8.compute-1.amazonaws.com/download/%s/\nMessage:%s''' % (fa.sender_email, str(af.id), message)
-		else:
-			fa.access_type = 'U'
-			fa.ariento_user = u
-			email_body = '''An Ariento client, %s, has sent you a file.\nLog in to download it:http://ec2-54-172-241-8.compute-1.amazonaws.com/login\nMessage:%s''' % (fa.sender_email, message)
-		finally:
-			fa.save()
+			fa = FileAccess()
+			fa.file = af
+			fa.recipient_email = recipient
+			fa.sender_email = email
+			try:
+				u = User.objects.get(email=recipient)
+			except ObjectDoesNotExist:
+				fa.access_type = 'P'
+				fa.salt = get_salt()
+				fa.hashed_password = hashed_password(password, fa.salt)
+				email_body = '''An Ariento client, %s, has sent you a file.\nClick here to download it:http://ec2-54-172-241-8.compute-1.amazonaws.com/download/%s/\nMessage:%s''' % (fa.sender_email, str(af.id), message)
+			else:
+				fa.access_type = 'U'
+				fa.ariento_user = u
+				email_body = '''An Ariento client, %s, has sent you a file.\nLog in to download it:http://ec2-54-172-241-8.compute-1.amazonaws.com/login\nMessage:%s''' % (fa.sender_email, message)
+				client_is_recipient = True
+			finally:
+				fa.save()
 
-			emailer.sendmail(recipient, "Ariento File Send", email_body)
+				if (client_is_recipient and not sent_to_client):
+					emailer.sendmail(recipient, "Ariento File Send", email_body)
+					sent_to_client = True
+				elif (client_is_recipient and sent_to_client):
+					pass
+				else:
+					emailer.sendmail(recipient, "Ariento File Send", email_body)
 
-			context = {
-				'recipient': recipient,
-				'num_files': af.id,
-			}
-			template = loader.get_template('success.html')
-			return HttpResponse(template.render(context, request))
+		context = {
+			'recipient': recipient,
+			'num_files': af.id,
+		}
+		template = loader.get_template('success.html')
+		return HttpResponse(template.render(context, request))
 
 def download(request, key):
 	try:
@@ -160,8 +180,7 @@ def guest_send(request):
 		sender = request.POST['guestEmail']
 		recipient = request.POST['email']
 		message = request.POST['message']
-		# only gets one file upload even if multiple are sent
-		attachment = request.FILES['attachments']
+		attachment = request.FILES.getlist('attachments')
 	except KeyError:
 		return render(request, 'guest.html', {})
 	else:
@@ -170,24 +189,25 @@ def guest_send(request):
 		except ObjectDoesNotExist:
 			return render(request, 'guest.html', {})
 		else:
-			af = ArientoFile()
-			af.file=attachment
-			af.save()
+			for attached in attachment:
+				af = ArientoFile()
+				af.file=attached
+				af.save()
 
-			fa = FileAccess()
-			fa.file = af
-			fa.access_type = 'U'
-			fa.ariento_user = u
-			fa.sender_email = sender
-			fa.recipient_email = recipient
-			fa.save()
+				fa = FileAccess()
+				fa.file = af
+				fa.access_type = 'U'
+				fa.ariento_user = u
+				fa.sender_email = sender
+				fa.recipient_email = recipient
+				fa.save()
 
 			email_body = '''An Ariento guest, %s, has sent you a file.\nClick here to download it:http://ec2-54-172-241-8.compute-1.amazonaws.com/download/%s/\nMessage:%s''' % (sender, str(af.id), message)
 			emailer.sendmail(recipient, "Ariento File Send", email_body)
 
 			context = {
 				'recipient': recipient,
-				'num_files': af.id,
+				'num_files': attachment,
 			}
 			template = loader.get_template('success.html')
 			return HttpResponse(template.render(context, request))
